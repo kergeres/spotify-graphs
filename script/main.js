78b35b7aeaa396265435dc11729339f3337c221c
kergeres <55786735+kergeres@users.noreply.github.com>
2021-12-14 21:38:14 +0100

    search special char to regular.searchbar animation

--- script/main.js
+++ script/main.js
@@ -34,8 +34,7 @@ const addToCompare = () => {
         newCard.innerHTML = `<p>${krubi}</p> <span>&#10005;</span>`
         document.querySelector(".card-container").appendChild(newCard)
     })
-    // btn.classList.remove("compare-bar")
-    // btn.style.width = "230px"
+
 }
 
 // addToCompare()
@@ -43,28 +42,37 @@ const addToCompare = () => {
 
 let tranformToSearch = () => {
 
-    let newItem = document.createElement("div")
-    newItem.classList.add("search-container")
-    newItem.style.backgroundColor = "rgb(209, 209, 209)"
-    newItem.innerHTML = `<input placeholder="Search" class="search-input"><span onclick="closeSearch" class="spanom">&#10005;</span>`
-    let oldItem = document.querySelector(".add-more-btn")
-    setTimeout(() => {
-        document.querySelector(".search-input").focus()
-    }, 10);
-    document.querySelector(".compare-container").replaceChild(newItem, oldItem)
-    search()
+    document.querySelector(".add-more-btn").addEventListener('click', () => {
+        let newItem = document.createElement("div")
+        newItem.classList.add("search-container")
+        newItem.style.backgroundColor = "rgb(209, 209, 209)"
+        newItem.innerHTML = `<input placeholder="Search" class="search-input"><span class="spanom">&#10005;</span>`
+        let oldItem = document.querySelector(".add-more-btn")
+        setTimeout(() => {
+            document.querySelector(".search-input").focus()
+
+        }, 10);
+        document.querySelector(".compare-container").replaceChild(newItem, oldItem)
+        search()
+    })
+
 }
+tranformToSearch()
 
-const closeSearch = () => {
-    let newItem = document.createElement("div")
-    newItem.classList.add("add-more-btn")
-    newItem.innerHTML = ` <p>add more</p> <span>&#10005;</span>`
-    let oldItem = document.querySelector("search-container")
-    document.querySelector(".compare-container").replaceChild(newItem, oldItem)
+let closeSearch = () => {
+    document.querySelector(".spanom").addEventListener("click", () => {
+        let oldItem = document.querySelector(".search-container")
+        let newItem = document.createElement("div")
+        newItem.setAttribute('onclick', "tranformToSearch()")
+
+        newItem.classList.add("add-more-btn")
+        newItem.innerHTML = ` <p>add more</p><span>&#10005;</span>`
+        document.querySelector(".compare-container").replaceChild(newItem, oldItem)
+        setTimeout(() => {
+            newItem.click()
+        }, 10);
+    })
 }
-
-
-
 
 let Append = (wholeDataBase) => {
     let htmlTemlate = ""
@@ -93,9 +101,9 @@ const search = () => {
     inputfield.addEventListener("keyup", () => {
         let filteredResults = [];
         for (const iti of dataStorage) {
-            let artistID = iti.artist.toLowerCase();
+            let artistID = iti.artist.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
             //   let carBrand = iti.initials.brand.toLowerCase();
-            if (artistID.includes(inputfield.value.toLowerCase())) {
+            if (artistID.includes(inputfield.value.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, ''))) {
                 filteredResults.push(iti);
             }
             //   else if (carBrand.includes(value.toLowerCase())) 
@@ -108,6 +116,7 @@ const search = () => {
         Append(filteredResults)
     })
 
+    closeSearch()
 }
 
 
