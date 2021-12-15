458f586b6671f5bd2985edf86013a935f0250780
kergeres <55786735+kergeres@users.noreply.github.com>
2021-12-15 20:16:37 +0100

    bizti save. search displayed in table; go w chart.js

--- script/main.js
+++ script/main.js
@@ -7,7 +7,7 @@ let dataStorage = []
 const getData = async () => {
     const response = await fetch(url)
     const data = await response.json()
-    Append(data)
+    // Append(data)                  ez a regi h1es append, torolt ezt es a funct is
     // save to global var
     dataStorage = data;
 }
@@ -17,62 +17,64 @@ getData()
 const addToCompare = (nameIn) => {
 
     // Append selected card to the compare bar
-    let barValues = localStorage.getItem("todos") != null ? localStorage.getItem("todos") : [];
-    if (!barValues.includes(nameIn.innerHTML)) {
+    let barValues = sessionStorage.getItem("toCompare") != null ? sessionStorage.getItem("toCompare") : [];
+
+    console.log(nameIn.parentElement.children[0].innerHTML);
+    if (!barValues.includes(nameIn.parentElement.children[0].innerHTML)) {
         let newCard = document.createElement("div")
         newCard.classList.add("compare-item-card")
-        newCard.innerHTML = `<p>${nameIn.innerHTML}</p> <span onclick="deleteFromCompare(this)" class="compareCardDeleteBtn">&#10005;</span>`;
-        saveToLocalStorage(nameIn.innerHTML);
+        newCard.innerHTML = `<p>${nameIn.parentElement.children[0].innerHTML}</p> <span onclick="deleteFromCompare(this)" class="compareCardDeleteBtn">&#10005;</span>`;
+        saveTosessionStorage(nameIn.parentElement.children[0].innerHTML);
         document.querySelector(".card-container").appendChild(newCard)
     }
 
 
 }
 
-// Delete cars from the compare par by click on X
+// Delete cards from the compare par by click on X
 const deleteFromCompare = (e) => {
     e.parentElement.remove()
-    deleteFromLocalstorage(e.parentElement)
+    deleteFromsessionStorage(e.parentElement)
 }
-
-const deleteFromLocalstorage = (todo) => {
-    let todos;
-    if (localStorage.getItem('todos') === null) {
-        todos = []
+// Delete cards from the sessionStorage
+const deleteFromsessionStorage = (item) => {
+    let toCompare;
+    if (sessionStorage.getItem('toCompare') === null) {
+        toCompare = []
     }
     else {
-        todos = JSON.parse(localStorage.getItem('todos'))
+        toCompare = JSON.parse(sessionStorage.getItem('toCompare'))
     }
-    let itemToRemoveIndex = todo.children[0].innerText;
-    console.log(todos);
-    todos.splice(todos.indexOf(itemToRemoveIndex), 1);
-    localStorage.setItem('todos', JSON.stringify(todos))
+    let itemToRemoveIndex = item.children[0].innerText;
+    console.log(toCompare);
+    toCompare.splice(toCompare.indexOf(itemToRemoveIndex), 1);
+    sessionStorage.setItem('toCompare', JSON.stringify(toCompare))
 }
 
 
 // save selected artists/albums etc to local storage 
-const saveToLocalStorage = (todo) => {
-    let todos;
-    if (localStorage.getItem('todos') === null) {
-        todos = []
+const saveTosessionStorage = (item) => {
+    let toCompare;
+    if (sessionStorage.getItem('toCompare') === null) {
+        toCompare = []
     }
     else {
-        todos = JSON.parse(localStorage.getItem('todos'))
+        toCompare = JSON.parse(sessionStorage.getItem('toCompare'))
     }
-    todos.push(todo)
-    localStorage.setItem('todos', JSON.stringify(todos))
+    toCompare.push(item)
+    sessionStorage.setItem('toCompare', JSON.stringify(toCompare))
 }
 // get and append the locasstorage values (saved artists/albums etc) to the DOM 
-const getFromLocalStorage = () => {
-    let todos;
-    if (localStorage.getItem('todos') === null) {
-        todos = []
+const getFromsessionStorage = () => {
+    let toCompare;
+    if (sessionStorage.getItem('toCompare') === null) {
+        toCompare = []
     }
     else {
-        todos = JSON.parse(localStorage.getItem('todos'))
+        toCompare = JSON.parse(sessionStorage.getItem('toCompare'))
     }
-    console.log(todos);
-    for (const iti of todos) {
+    console.log(toCompare);
+    for (const iti of toCompare) {
         let newCard = document.createElement("div")
         newCard.classList.add("compare-item-card")
         newCard.innerHTML = `<p>${iti}</p> <span onclick="deleteFromCompare(this)" class="compareCardDeleteBtn">&#10005;</span>`;
@@ -80,7 +82,7 @@ const getFromLocalStorage = () => {
 
     }
 }
-getFromLocalStorage()
+getFromsessionStorage()
 
 
 // animation to searchbar to appear
@@ -89,7 +91,24 @@ let tranformToSearch = () => {
         let newItem = document.createElement("div")
         newItem.classList.add("search-container")
         newItem.style.backgroundColor = "rgb(209, 209, 209)"
-        newItem.innerHTML = `<input placeholder="Search" class="search-input"><span class="spanom">&#10005;</span>`
+        newItem.innerHTML = `
+
+        <table class="search-table">
+            <thead>
+                <tr>
+                    <th><input placeholder="Search" class="search-input"></th>
+                    <th><span class="spanom">&#10005;</span></th>
+                </tr>
+            </thead>
+            <tbody id="stable">
+
+            </tbody>
+                
+        </table>
+        
+        
+        
+        `
         let oldItem = document.querySelector(".add-more-btn")
         setTimeout(() => {
             document.querySelector(".search-input").focus()
@@ -132,10 +151,13 @@ let Append = (wholeDataBase) => {
 let appendSerch = (filtered) => {
     let htmlTemlate = ""
     for (let iterator of filtered) {
-        htmlTemlate += `<h1 onclick="appendThis(this)">${iterator.artist}</h1>`
-        console.log(iterator.artist);
+        htmlTemlate += `
+                <tr>
+                    <td>${iterator.artist}</td>
+                    <td onclick="addToCompare(this)" ><i  class="far fa-plus-square"></i></td>
+                </tr>`
     }
-    document.querySelector("#scene").innerHTML = htmlTemlate;
+    document.querySelector("#stable").innerHTML = htmlTemlate;
 }
 
 // search in for artist,albums etc in the database
@@ -157,12 +179,69 @@ const search = () => {
             //   }
         }
 
-        console.log(inputfield.value);
-        Append(filteredResults)
+        appendSerch(filteredResults)
     })
 
     closeSearch()
 }
+
+
+
+
+
+
+
+
+
+
+
+let compare = () => {
+
+    let sStorage = sessionStorage.getItem("toCompare")
+    console.log(sStorage);
+    const ctx = document.getElementById('graph-container').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: sStorage,
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+
+}
+
+
+
+
+
 
 
 
